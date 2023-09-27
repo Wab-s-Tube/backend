5c6597a94e9b16a74ecdf6d5bc5da15de828cf09
wabowen89 <wabowen89@gmail.com>
2023-09-27 14:13:36 -0700

    added seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,51 @@
 
 user1 = User.where(email: "test1@example.com").first_or_create(password: "password", password_confirmation: "password")
 user2 = User.where(email: "test2@example.com").first_or_create(password: "password", password_confirmation: "password")
+
+
+videos_first = [
+    {
+        file_name: "First Steps",
+        size: 250,
+        duration: 60,
+        mime_type: "mpa"
+    },
+       {
+        file_name: "My bike",
+        size: 365,
+        duration: 123,
+        mime_type: "mpa"
+    },
+]
+
+videos_second = [
+    {
+        file_name: "How to Drive",
+        size: 2764,
+        duration: 195,
+        mime_type: "mpa"
+    },
+       {
+        file_name: "Jackson Industry",
+        size: 3489,
+        duration: 486,
+        mime_type: "mpa"
+    },
+
+    {
+        file_name: "Little John",
+        size: 31849,
+        duration: 42746,
+        mime_type: "mpa"
+    },
+]
+
+videos_first.each do |video|
+    user1.videos.create(video)
+    p "creating video #{video}"
+end
+
+videos_second.each do |video|
+    user2.videos.create(video)
+    p "creating video #{video}"
+end
